510c1701ff944badb371f5913af44a8b8a1107d5
fruty809 <brawlstarsvoron809@gmail.com>
2023-07-07 20:45:05 +0600

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] array = {-7, -4, -2, 2, 3, 6, 8};
+
+        System.out.println("Исходный массив: ");
+        printArray(array);
+
+        //Алгоритм сортировки
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                double temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+            System.out.println("Текущее состояние массива после итерации " + (i + 1) + ":");
+            printArray(array);
+        }
+        System.out.println("Отсортированный массив:");
+        printArray(array);
+    }
+    public static void printArray(double[]array){
+        for (double num : array){
+            System.out.println(num +"");
+        }
+        System.out.println();
     }
 }
