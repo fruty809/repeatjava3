0e8ca57e18de89dd0e63272a840ed93e6d307028
fruty809 <brawlstarsvoron809@gmail.com>
2023-07-07 22:39:30 +0600

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        double[] array = {-7, -4, -2, 2, 3, 6, 8};
+        // Основное задание
+        double[] numbers = {8, -2, -4, 2, 3, 6, -7};
 
-        System.out.println("Исходный массив: ");
-        printArray(array);
+        boolean negativeNumberFound = false;
+        double sum = 0;
+        int count = 0;
 
-        //Алгоритм сортировки
+        for (double number : numbers) {
+            if (negativeNumberFound) {
+                if (number > 0) {
+                    sum += number;
+                    count++;
+                }
+            } else if (number < 0) {
+                negativeNumberFound = true;
+            }
+        }
+
+        double average = sum / count;
+        System.out.println("Среднее арифметическое положительных чисел после первого отрицательного числа: " + average);
+
+        // Задание на сообразительность
+        int[] array = {-7, -4, -2, 2, 3, 6, 8};
+
         for (int i = 0; i < array.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < array.length; j++) {
@@ -13,20 +31,17 @@ public class Main {
                     minIndex = j;
                 }
             }
-            if (minIndex != i) {
-                double temp = array[i];
-                array[i] = array[minIndex];
-                array[minIndex] = temp;
-            }
-            System.out.println("Текущее состояние массива после итерации " + (i + 1) + ":");
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+
             printArray(array);
         }
-        System.out.println("Отсортированный массив:");
-        printArray(array);
     }
-    public static void printArray(double[]array){
-        for (double num : array){
-            System.out.println(num +"");
+
+    private static void printArray(int[] array) {
+        for (int number : array) {
+            System.out.print(number + " ");
         }
         System.out.println();
     }
